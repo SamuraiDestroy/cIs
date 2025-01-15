1b8a06f5cfbf412f488e095ef2d5d1986fea6eed
SamuraiDestroy <01PMvictor@gmail.com>
2025-01-15 15:14:13 +0100

    squar

---
+++ thing.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    printf("hope you like squares\n");
+    printf("____\n");
+    printf("|   |\n");
+    printf("|   |\n");
+    printf("|___|\n");
+
+    return 0;
+}
