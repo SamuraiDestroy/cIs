1153d2d4fac97b6a07ca3500ef3c38e93d384746
SamuraiDestroy <01PMvictor@gmail.com>
2025-01-15 15:24:14 +0100

    tiny fix

--- thing.c
+++ thing.c
@@ -3,12 +3,12 @@ int main() {
     char shape[] = "square";
     int sides = 4;
 
-    printf("hope you like %s\n", shape);
+    printf("hope you like %ss\n", shape);
     printf("____\n");
     printf("|   |\n");
     printf("|   |\n");
     printf("|___|\n");
-    printf("it should have %s sides", sides);
+    printf("it should have %i sides", sides);
 
     return 0;
 }
