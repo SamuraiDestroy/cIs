3ed2c6f32b77a547fdf82a76a4ef695dae36e9db
SamuraiDestroy <01PMvictor@gmail.com>
2025-01-15 15:23:08 +0100

    shape time

--- thing.c
+++ thing.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 int main() {
-    printf("hope you like squares\n");
+    char shape[] = "square";
+    int sides = 4;
+
+    printf("hope you like %s\n", shape);
     printf("____\n");
     printf("|   |\n");
     printf("|   |\n");
     printf("|___|\n");
+    printf("it should have %s sides", sides);
 
     return 0;
 }
