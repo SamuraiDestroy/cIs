60772d9d5f3218a6997ccb08b4d1106cf2a51cf6
SamuraiDestroy <01PMvictor@gmail.com>
2025-01-18 08:17:50 +0100

    Added some comments

--- thing.c
+++ thing.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+
 int main() {
     char shape[] = "square";
     int sides = 4;
+    // Very important variables that will definitely get changed
 
     printf("hope you like %ss\n", shape);
     printf("____\n");
@@ -11,4 +13,5 @@ int main() {
     printf("it should have %i sides", sides);
 
     return 0;
+    // That means it worked
 }
